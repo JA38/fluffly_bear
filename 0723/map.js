504e7a7414f0a95d45c21148962b3aa63cd77eed
JA38 <qwertycat49@korea.ac.kr>
2020-07-27 18:08:35 +0900

    API 활용하여 지도 서비스 구현하기

---
+++ 0723/map.js
@@ -0,0 +1,112 @@
+
+
+// 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 생성합니다    
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places();
+
+
+function btn() {
+    let my_keyword = document.getElementById('keyword').value;
+    console.log("btn()working,,,", my_keyword)
+    // 키워드로 장소를 검색합니다
+    ps.keywordSearch(my_keyword, placesSearchCB);
+
+    // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+    function placesSearchCB(data, status, pagination) {
+        if (status === kakao.maps.services.Status.OK) {
+
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+            // LatLngBounds 객체에 좌표를 추가합니다
+            var bounds = new kakao.maps.LatLngBounds();
+
+            for (var i = 0; i < data.length; i++) {
+                displayMarker(data[i]);
+                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+            }
+
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+            map.setBounds(bounds);
+        }
+    }
+
+    let keyword = document.getElementById('keyword')
+    keyword.value = ' '
+
+}
+
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x)
+    });
+
+    var content = '<div class="wrap">' +
+        '    <div class="info">' +
+        '        <div class="title">' +
+        place.place_name +
+        '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
+        '        </div>' +
+        '        <div class="body">' +
+        '            <div class="desc">' +
+        '                <div class="ellipsis">' + place.address_name + '</div>' +
+        '                <div class="phone">' + place.phone + '</div>' +
+        '                <div><a href=' + place.place_url + ' target="_blank" class="link">상세페이지</a></div>' +
+        '            </div>' +
+        '        </div>' +
+        '    </div>' +
+        '</div>';
+    // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+    var overlay = new kakao.maps.CustomOverlay({
+        content: content,
+        map: map,
+        position: marker.getPosition()
+    });
+
+    // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+    kakao.maps.event.addListener(marker, 'click', function () {
+        overlay.setMap(map);
+    });
+
+    // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
+
+    function closeOverlay() {
+        overlay.setMap(null);
+    }
+    closeOverlay();
+}
+
+
+
+function resizeMap() {
+    var mapContainer = document.getElementById('map');
+    mapContainer.style.width = '100%';
+    mapContainer.style.height = '100rem';
+}
+
+function relayout() {
+
+    // 지도를 표시하는 div 크기를 변경한 이후 지도가 정상적으로 표출되지 않을 수도 있습니다
+    // 크기를 변경한 이후에는 반드시  map.relayout 함수를 호출해야 합니다 
+    // window의 resize 이벤트에 의한 크기변경은 map.relayout 함수가 자동으로 호출됩니다
+    map.relayout();
+}
+
+resizeMap()
+relayout()
+
+
